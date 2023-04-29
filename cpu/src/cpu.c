0154c47eff4fedc91b57f0984d7a26551e1e5c7e
Francisca María Olaciregui Bonilla <folacireguibonilla@frba.utn.edu.ar>
2023-04-29 18:36:57 +0000

    Correccion conexiones

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -6,10 +6,11 @@ int main(int argc, char** argv)
     t_log* logger = iniciar_logger(DEFAULT_LOG_PATH, ENUM_CPU);
     t_config* config = iniciar_config(DEFAULT_CONFIG_PATH, logger);
 
+    int servidorCpu = iniciar_servidor(config, logger);
+
     // Conexion con memoria
     int conexionMemoria = armar_conexion(config, MEMORIA, logger);
 
-    int servidorCpu = iniciar_servidor(config, logger);
     int clienteAceptado = esperar_cliente(servidorCpu, logger);
 
     terminar_programa(servidorCpu, logger, config);
