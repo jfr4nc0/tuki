07bfff27be44eecadb9b0c01e2da98efc5fdc4df
Joako <joakosoaje@gmail.com>
2023-05-13 19:45:36 +0000

    eliminando innecesaria parte del codigo

--- shared/funciones.h
+++ shared/funciones.h
@@ -10,7 +10,6 @@ char* extraer_de_modulo_config(t_config*, char*, char*, t_log* logger);
 char* concatenar_strings(char*, char*);
 t_log* iniciar_logger(char*, int);
 t_config* iniciar_config(char*, t_log*);
-void leer_consola(t_log*);
 void terminar_programa(int, t_log*, t_config*);
 void liberar_conexion(int);
 bool obtener_valores_para_logger(int, bool*, t_log_level*, char**);
