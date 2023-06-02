79926c474ca046db285cc5acc1d69c274aed4874
Joan Canossa <jfcpersonal99@gmail.com>
2023-06-02 04:21:30 +0000

    Ambiente OK

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -322,3 +322,48 @@ void instruccion_set(char* registro,char* valor) {
 	usleep(atoi(configCpu->RETARDO_INSTRUCCION)*1000);
 }
 
+void instruccion_mov_in(char* registro,char* dir_logica){
+
+}
+void instruccion_mov_out(char* dir_logica,char* registro){
+
+}
+void instruccion_io(char* tiempo){
+
+}
+void instruccion_f_open(char* nombre_archivo){
+
+}
+void instruccion_f_close(char* nombre_archivo){
+
+}
+void instruccion_f_seek(char* nombre_archivo, char* posicion){
+
+}
+void instruccion_f_read(char* nombre_archivo, char* dir_logica, char* cant_bytes){
+
+}
+void instruccion_f_write(char* nombre_archivo, char* dir_logica, char* cant_bytes){
+
+}
+void instruccion_f_truncate(char* nombre_archivo,char* tamanio){
+
+}
+void instruccion_wait(char* recurso){
+
+}
+void instruccion_signal(char* recurso){
+
+}
+void instruccion_create_segment(char* id_segmento, char* tamanio){
+
+}
+void instruccion_delete_segment(char* id_segmento){
+
+}
+void instruccion_yield(){
+
+}
+void instruccion_exit(){
+
+}
