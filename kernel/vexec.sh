75b8d80ada09405c5473d06ff17a67140fc7fc74
Joan Canossa <jfcpersonal99@gmail.com>
2023-06-02 21:45:14 +0000

    Nuevo ambiente base OK

--- kernel/vexec.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-FILE=kernel
-DIR=./Debug
-
-if test -f "$DIR/$FILE"; then
-    cd $DIR
-    valgrind --tool=memcheck --leak-check=yes --show-possibly-lost=no --show-reachable=no --num-callers=20 ./$FILE
-fi
---
+++ modules/consola/src/main.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <utils/hello.h>
+
+int main(int argc, char* argv[]) {
+    puts("Hello world!!");
+    return 0;
+}
