7a75181653970a7479d6b114ef2a928ff246534b
Joako <joakosoaje@gmail.com>
2023-06-07 04:48:25 +0000

    modified text on printf

--- modules/utils/src/shared/shared.c
+++ modules/utils/src/shared/shared.c
@@ -168,7 +168,7 @@ t_log* iniciar_logger(char* pathLog, int moduloPos) {
 t_config* iniciar_config(char* pathConfig, t_log* logger) {
     t_config* nuevo_config;
     char *directorioActual = get_current_dir_name();
-    printf("Por crear logger desde %s \n", directorioActual);
+    printf("Por crear config desde %s \n", directorioActual);
     free(directorioActual);
     if ((nuevo_config = config_create(pathConfig)) == NULL) {
         log_error(logger, E__CONFIG_CREATE);
